5a6543e3d06014f79068ee06e947ca1c922f7ab0
Dmitrii Boikov <boikovdv@gmail.com>
2022-09-15 19:04:42 +0300

    initial fixes new polylib version

--- components/init.js
+++ components/init.js
@@ -1,6 +1,3 @@
-import { Template } from 'polylib/template.js';
-Template.devMode = true;
-
 import "@editor/components/editor-launcher.js"
 const cont = document.createElement('pl-editor-launcher');
 document.body.appendChild(cont);
